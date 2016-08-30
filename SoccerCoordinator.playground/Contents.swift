b8e9bdbd4a11cf2f0f334eeefcc9b931ed54c99a
Richard Purba <richardtpurba@richards-mac-mini.local>
2016-08-30 13:09:59 +0200

    Using Playground. Attempting to finish Part1 and Part2

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,31 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+var nameRecord = ["Joe Smith","Jill Tanner","Bill Bon","Eva Gordon","Matt Gill","Kimmy Stein","Sammy Adams","Karl Saygan","Suzane Greenberg","Sal Dali","Joe Kavalier","Ben Finkelstein","Diego Soto","Chloe Alaska","Arnold Willis","Phillip Helm","Les Clay","Herschel Krustofski"]
+var heightRecord = [42,36,43,45,40,41,45,42,44,41,39,44,41,47,43,44,42,45]
 
-var str = "Hello, playground"
+var soccerExperienceRecord  = [true, true, true, false, false, false, false, true, true, false, false, false,true,false,false,true,true,true]
+var guardianNameRecord = ["Jim and Jan Smith","Clara Tanner","Sara nd Jenny Bon","Wendy and Mike Gordon","Charles and Sylvia Gill","Bill and Hillary Stein","Jeff Adams","Heather Bledsoe","Henrietta Dumas","Gala Dali","Sam and Elaine Kavalier","Aaron and Jill Finkelstein","Robin and Sarika Soto","David and Jamie Alaska","Claire Willis","Thomas Helm and Eva Jones","Wynonna Brown","Hyman and Rachel Krustofski"]
 
-// Gabe's comment
+let numberOfData = nameRecord.count//checking the number of inputs for name
+heightRecord.count//checking for height
+soccerExperienceRecord.count//checking for soccerExperienceRecord
+guardianNameRecord.count
+
+
+func returningSoccerExperience(input: String)-> Bool{
+    var nameMatched = ""
+    var indexRecordForFunction = 0
+    for index in 0..<numberOfData{
+        if input == nameRecord[index]{
+             nameMatched  = input
+             indexRecordForFunction = index
+        }
+    }
+    switch input{
+        
+        
+    case nameMatched : return soccerExperienceRecord[indexRecordForFunction]
+    default : return false// the return is ambiguous here..
+    }
+}
+returningSoccerExperience("Eva Gordon")//testing
