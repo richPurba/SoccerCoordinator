27be19f16db09d465cec7456ed3b2c2cbb1e7752
Richard Purba <richardtpurba@richards-mac-mini.local>
2016-09-10 18:45:17 +0200

    I think this is done. But i will do the last check before submitting to treeHouse

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -4,7 +4,7 @@ var nameRecord = ["Joe Smith","Jill Tanner","Bill Bon","Eva Gordon","Matt Gill",
 var heightRecord = [42,36,43,45,40,41,45,42,44,41,39,44,41,47,43,44,42,45]
 
 var soccerExperienceRecord  = [ true, true,true, false, false, false, false, true, true, false, false, false,true,false,false,true,true,true]
-var guardianNameRecord = ["Jim and Jan Smith","Clara Tanner","Sara nd Jenny Bon","Wendy and Mike Gordon","Charles and Sylvia Gill","Bill and Hillary Stein","Jeff Adams","Heather Bledsoe","Henrietta Dumas","Gala Dali","Sam and Elaine Kavalier","Aaron and Jill Finkelstein","Robin and Sarika Soto","David and Jamie Alaska","Claire Willis","Thomas Helm and Eva Jones","Wynonna Brown","Hyman and Rachel Krustofski"]
+var guardianNameRecord = ["Jim and Jan Smith","Clara Tanner","Sara and Jenny Bon","Wendy and Mike Gordon","Charles and Sylvia Gill","Bill and Hillary Stein","Jeff Adams","Heather Bledsoe","Henrietta Dumas","Gala Dali","Sam and Elaine Kavalier","Aaron and Jill Finkelstein","Robin and Sarika Soto","David and Jamie Alaska","Claire Willis","Thomas Helm and Eva Jones","Wynonna Brown","Hyman and Rachel Krustofski"]
 
 let numberOfData = nameRecord.count//checking the number of inputs for name
 heightRecord.count//checking for height
@@ -55,7 +55,7 @@ func returningAverageHeightOfTeam(nameOfTeam: [String])-> Float{
     var heightOfIndividual = [Int]()
     
     for index in 0..<nameOfTeam.count{
-        var matchingValues = nameOfTeam[index]
+        var matchingValues = nameOfTeam[index]// not sure why i have to change this to 'let', consider the value will change. Is it because func type?
         
         for index2 in 0..<nameRecord.count{
             if matchingValues == nameRecord[index2]{
@@ -83,9 +83,13 @@ var teamSharks = [String]()
 var teamRaptors = [String]()
 let numberOfTeam = 3 // this is an input
 let thresholdOfHeight: Float = 1.5
+var pseudoStoringExperiencedPlayers = storingExperiencedPlayers
+var pseudoStoringNonExperiencedPlayers = storingNonExperiencedPlayers
 
 //creating a divisor so that the division will be evenly distributed
 let divisorOfExperiencedPlayers = countingExperiencedPlayer/numberOfTeam
+let divisorOfNonExperiencedPlayers = countingNonExperiencedPlayer/numberOfTeam
+let maximumPlayerPerTeam = nameRecord.count / numberOfTeam
 
 //initializing the threshold logic variables, initializing them higher then the threshold
 var differenceAverageHeighetDragonAndSharks: Float = thresholdOfHeight + 1
@@ -97,49 +101,132 @@ else {
  
      // creating difference average values of the function
      //starting to make it higher than the threshold first until it is violated (i.e., to make it lower than the threshold)
-    
      
     while ((differenceAverageHeighetDragonAndSharks > thresholdOfHeight) || (differenceAverageHeighetDragonAndRaptors > thresholdOfHeight) || (differenceAverageHeighetSharksAndRaptors > thresholdOfHeight)){
         /// putting the experienced players randomly
-        for indexAgain in 0..<storingExperiencedPlayers.count {
-            var pickingThePlayerFromRecord = storingExperiencedPlayers[indexAgain]//picking always the first index
+        
+        while pseudoStoringExperiencedPlayers.count != 0 {
+            var pickingThePlayerFromRecord = pseudoStoringExperiencedPlayers[0]//picking always the first index
             
             var countingRandomPick = Int(arc4random() % uint(numberOfTeam) + 1)
+            
             if ((countingRandomPick == 1) && (teamDragon.count < divisorOfExperiencedPlayers) ){
                 teamDragon.append(pickingThePlayerFromRecord)//deleting always the first index after restoring it
+                pseudoStoringExperiencedPlayers.removeAtIndex(0)
             }
             else if  ((countingRandomPick == 2 ) && (teamSharks.count < divisorOfExperiencedPlayers)) {
                 teamSharks.append(pickingThePlayerFromRecord)//deleting always the first index after restoring it
+                pseudoStoringExperiencedPlayers.removeAtIndex(0)
             }
             else if ((countingRandomPick == 3) && (teamRaptors
                 .count < divisorOfExperiencedPlayers)){
                 teamRaptors.append(pickingThePlayerFromRecord)//deleting always the first index after restoring it
+                pseudoStoringExperiencedPlayers.removeAtIndex(0)
             }
             
         }// end of putting the experienced players randomly
-        teamRaptors
         
-        let divisorOfNonExperiencedPlayers = countingNonExperiencedPlayer/numberOfTeam
         
-         for indexAgain in 0..<storingNonExperiencedPlayers.count {
+        // assigning nonExperienced Players
+         while pseudoStoringNonExperiencedPlayers.count != 0 {
             
-            var pickingThePlayerFromRecord = storingNonExperiencedPlayers[indexAgain]
+            var pickingThePlayerFromRecord = pseudoStoringNonExperiencedPlayers[0]
             
             var countingRandomPick = Int(arc4random() % uint(numberOfTeam) + 1)
             
-            if((countingRandomPick == 1) && (teamDragon.count <= teamDragon.count + divisorOfNonExperiencedPlayers)){
+            if((countingRandomPick == 1) && (teamDragon.count < maximumPlayerPerTeam)){
                 teamDragon.append(pickingThePlayerFromRecord)
-            }else if((countingRandomPick == 2) && (teamSharks.count <= teamSharks.count + divisorOfNonExperiencedPlayers)){
+                pseudoStoringNonExperiencedPlayers.removeAtIndex(0)
+            }else if((countingRandomPick == 2) && (teamSharks.count < maximumPlayerPerTeam)){
                 teamSharks.append(pickingThePlayerFromRecord)
-            }else if((countingRandomPick == 3) && (teamRaptors.count <= teamRaptors.count + divisorOfNonExperiencedPlayers)){
+                pseudoStoringNonExperiencedPlayers.removeAtIndex(0)
+            }else if((countingRandomPick == 3) && (teamRaptors.count < maximumPlayerPerTeam)){
                 teamRaptors.append(pickingThePlayerFromRecord)
+                pseudoStoringNonExperiencedPlayers.removeAtIndex(0)
             }
             
         }
+        //redefining the logic of threshold here, so the violation of the threshold will result in removing the array's values
         
         differenceAverageHeighetDragonAndSharks = (abs((returningAverageHeightOfTeam(teamDragon) - returningAverageHeightOfTeam(teamSharks))))
         differenceAverageHeighetDragonAndRaptors = abs(returningAverageHeightOfTeam(teamDragon) - returningAverageHeightOfTeam(teamRaptors))
         differenceAverageHeighetSharksAndRaptors = abs(returningAverageHeightOfTeam(teamSharks) - returningAverageHeightOfTeam(teamRaptors))
         
+        if ((differenceAverageHeighetDragonAndSharks > thresholdOfHeight) || (differenceAverageHeighetDragonAndRaptors > thresholdOfHeight) || (differenceAverageHeighetSharksAndRaptors > thresholdOfHeight)){
+            // if the threshold is not satisfied, restart the collection from square one again
+            pseudoStoringExperiencedPlayers = storingExperiencedPlayers
+            pseudoStoringNonExperiencedPlayers = storingNonExperiencedPlayers
+            //removing the team
+            teamDragon.removeAll()
+            teamSharks.removeAll()
+            teamRaptors.removeAll()
+        }
+        
         }// end of threshold logic
+    
+    // continue the loop until satisfies the condition
    }
+
+//checking the array
+
+teamDragon.count
+teamDragon
+teamRaptors.count
+teamRaptors
+teamSharks.count
+teamSharks
+
+//checking the height's difference
+differenceAverageHeighetDragonAndSharks
+differenceAverageHeighetDragonAndRaptors
+differenceAverageHeighetSharksAndRaptors
+
+// function that returns the guardian, copy-pasted from func returningAverageHeightOfTeam()
+
+func returningGuardianOfTeam(nameOfTeam: [String])-> [String]{
+    var guardianNameOfIndividual = [String]()
+    
+    for index in 0..<nameOfTeam.count{
+        var matchingValues = nameOfTeam[index]// not sure why i have to change this to 'let', consider the value will change. Is it because func type?
+        for index2 in 0..<guardianNameRecord.count{
+            if matchingValues == nameRecord[index2]{
+                guardianNameOfIndividual.append(guardianNameRecord[index2])
+            }
+            
+        }// end of for level 2
+        
+        
+    }// end of for level 1
+    
+    return guardianNameOfIndividual
+}// end of function
+
+// making dictionary to ease the loop for the letter to the guardian
+
+var dictionaryForTeam  = [0: teamDragon,1:teamSharks,2:teamRaptors]
+
+for index in 0..<numberOfTeam{
+    var temporaryIndividualName = dictionaryForTeam[index]!
+    // sorry for this, i am aware of the Optional type (to a very minimum extent!), although it hasn't been covered by the lecture so far. 
+    // so i have to unwrap it. I know this might result in crash. Again, i don't know to what extent it will crash
+    var temporaryGuardianName = returningGuardianOfTeam(temporaryIndividualName)
+    
+    var teamPracticeDatesLiteral = ""
+    var nameTeamLiteral = ""
+    var messageBody : String = ""
+    switch index {
+    case 0 : teamPracticeDatesLiteral = "March 17, 1pm"
+                nameTeamLiteral = "Team Dragon"
+    case 1 : teamPracticeDatesLiteral = "March 17, 3pm"
+                nameTeamLiteral = "Team Sharks"
+    case 2 :teamPracticeDatesLiteral = "March 18, 1pm"
+                nameTeamLiteral = "Team Raptors"
+    default : "NOT AVAILABLE"
+        
+    }
+    for id in 0..<temporaryGuardianName.count{
+        messageBody = "Dear \(temporaryGuardianName[id]).\n" +
+            " \t\(temporaryIndividualName[id]) is assigned for \(nameTeamLiteral) for the soccer game. This team has to meet up at \(teamPracticeDatesLiteral). We are expecting \(temporaryIndividualName[id]) to be on time for this first team practice, so don't forget to remind your child! Looking forward to seeing \(temporaryIndividualName[id]) \n Thank you."
+    }
+    
+}
